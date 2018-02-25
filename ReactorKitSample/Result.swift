2ff2de946b6ae31c5169b52d7027b84f807f7864
shizunaito <shizuna.itoh@gmail.com>
2018-02-25 17:01:04 +0900

    add GitHubClient

---
+++ ReactorKitSample/Result.swift
@@ -0,0 +1,20 @@
+//
+//  Result.swift
+//  ReactorKitSample
+//
+//  Created by 伊藤静那(Ito Shizuna) on 2018/02/25.
+//  Copyright © 2018年 ShizunaIto. All rights reserved.
+//
+
+enum Result<T, Error : Swift.Error> {
+    case success(T)
+    case failure(Error)
+    
+    init(value: T) {
+        self = .success(value)
+    }
+    
+    init(error: Error) {
+        self = .failure(error)
+    }
+}
