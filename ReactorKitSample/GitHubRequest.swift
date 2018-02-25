ec165e00a05536f011167058c126bdd206fa6fcc
shizunaito <shizuna.itoh@gmail.com>
2018-02-25 16:24:22 +0900

    convert GitHubRequest -> URLRequest

--- ReactorKitSample/GitHubRequest.swift
+++ ReactorKitSample/GitHubRequest.swift
@@ -21,4 +21,21 @@ extension GitHubRequest {
     var baseURL: URL {
         return URL(string: "https://api.github.com/")!
     }
+    
+    func buildURLRequest() -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        switch method {
+        case .get:
+            components?.queryItems = queryItems
+        default:
+            fatalError("Unsupported method \(method)")
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.url = components?.url
+        urlRequest.httpMethod = method.rawValue
+        
+        return urlRequest
+    }
 }
